36d50b3cd11b23071a3bad84da9ebc46d9c05730
Lakshya A Agrawal <lakshyaaagrawal@berkeley.edu>
2026-02-12 12:27:32 -0800

    Ruff

--- src/gepa/__init__.py
+++ src/gepa/__init__.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2025 Lakshya A Agrawal and the GEPA contributors
 # https://github.com/gepa-ai/gepa
 
+from . import (
+    optimize_anything,  # expose submodule; use `from gepa.optimize_anything import optimize_anything` for the function
+)
 from .adapters import default_adapter
 from .api import optimize
-from . import optimize_anything  # expose submodule; use `from gepa.optimize_anything import optimize_anything` for the function
 from .core.adapter import EvaluationBatch, GEPAAdapter
 from .core.result import GEPAResult
 from .examples import aime
